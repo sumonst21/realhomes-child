1796013ba0e55b4f8397ab57fa855797cfa04d43
Md. Sumon Islam <developer.sumon@gmail.com>
2018-12-12 01:19:00 +0600

    Update single-product.php

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -79,7 +79,53 @@ $is_auction_property = get_post_meta($post->ID, 'REAL_HOMES_auction_enabled', tr
 
 					<div class="rh_page rh_page--fullWidth">
 
-						<?php get_template_part('assets/modern/partials/property/single/head'); ?>
+						<?php //get_template_part('assets/modern/partials/property/single/head'); ?>
+<?php
+/**
+ * Head of the single property template.
+ *
+ * @package    realhomes
+ * @subpackage modern
+ */
+
+global $post;
+
+?>
+
+<div class="rh_page__head rh_page__property">
+
+	<div class="rh_page__property_title">
+
+		<h1 class="rh_page__title">
+			<?php the_title(); ?>
+		</h1>
+		<!-- /.rh_page__title -->
+
+		<?php
+       // $address_display  = get_option('inspiry_display_property_address', 'true');
+       // $property_address = get_post_meta(get_the_ID(), 'REAL_HOMES_property_address', true);
+
+       // if ('true' === $address_display) {
+            ?>
+			<p class="rh_page__property_address">
+				<?php //echo esc_html($property_address); ?>
+			</p>
+
+			<?php
+            //$display_property_breadcrumbs = get_option('theme_display_property_breadcrumbs');
+            //if ('true' == $display_property_breadcrumbs) {
+             //   get_template_part('common/partials/breadcrumbs');
+            //} ?>
+			<!-- /.rh_page__property_address -->
+			<?php
+        //}
+        ?>
+
+	</div>
+	<!-- /.rh_page__property_title -->
+
+</div>
+<!-- /.rh_page__head -->
 
 
 						<div class="rh_property">
