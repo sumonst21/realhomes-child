9151909278875888926471f52805fecacb69299d
Md. Sumon Islam <developer.sumon@gmail.com>
2018-12-12 04:41:14 +0600

    Update bid.php

--- woocommerce/single-product/bid.php
+++ woocommerce/single-product/bid.php
@@ -18,22 +18,22 @@ $gmt_offset = get_option('gmt_offset') > 0 ? '+'.get_option('gmt_offset') : get_
 
 ?>
 	
-<p class="auction-condition"><?php echo apply_filters('conditiond_text', __( 'Item condition:', 'wc_simple_auctions' ), $product); ?><span class="curent-bid"> <?php  _e($product->get_condition(),'wc_simple_auctions' )  ?></span></p>
+<p style="display: none;" class="auction-condition"><?php echo apply_filters('conditiond_text', __( 'Item condition:', 'wc_simple_auctions' ), $product); ?><span class="curent-bid"> <?php  _e($product->get_condition(),'wc_simple_auctions' )  ?></span></p>
 
 <?php if(($product->is_closed() === FALSE ) and ($product->is_started() === TRUE )) : ?>			
 		
-	<div class="auction-time" id="countdown"><?php echo apply_filters('time_text', __( 'Time left:', 'wc_simple_auctions' ), $product_id); ?> 
+	<div class="auction-time bid-stats" id="countdown"><?php echo apply_filters('time_text', __( 'Time left:', 'wc_simple_auctions' ), $product_id); ?> 
 		<div class="main-auction auction-time-countdown" data-time="<?php echo $product->get_seconds_remaining() ?>" data-auctionid="<?php echo $product_id ?>" data-format="<?php echo get_option( 'simple_auctions_countdown_format' ) ?>"></div>
 	</div>
 
-	<div class='auction-ajax-change' >
+	<div class='auction-ajax-change bid-stats' >
 	    
 		<p class="auction-end"><?php echo apply_filters('time_left_text', __( 'Auction ends:', 'wc_simple_auctions' ), $product); ?> <?php echo  date_i18n( get_option( 'date_format' ),  strtotime( $product->get_auction_end_time() ));  ?>  <?php echo  date_i18n( get_option( 'time_format' ),  strtotime( $product->get_auction_end_time() ));  ?> <br />
 			<?php printf(__('Timezone: %s','wc_simple_auctions') , get_option('timezone_string') ? get_option('timezone_string') : __('UTC ','wc_simple_auctions').$gmt_offset) ?>
 		</p>
 
 		<?php if ($product->get_auction_sealed() != 'yes'){ ?>
-		    <p class="auction-bid"><?php echo $product->get_price_html() ?> </p>
+		    <p class="auction-bid bid-stats"><?php echo $product->get_price_html() ?> </p>
 			
 			<?php if(($product->is_reserved() === TRUE) &&( $product->is_reserve_met() === FALSE )  ) : ?>
 				<p class="reserve hold"  data-auction-id="<?php echo esc_attr( $product_id ); ?>" ><?php echo apply_filters('reserve_bid_text', __( "Reserve price has not been met", 'wc_simple_auctions' )); ?></p>
@@ -106,7 +106,7 @@ $gmt_offset = get_option('gmt_offset') > 0 ? '+'.get_option('gmt_offset') : get_
 
 <?php elseif (($product->is_closed() === FALSE ) and ($product->is_started() === FALSE )):?>
 	
-	<div class="auction-time future" id="countdown"><?php echo apply_filters('auction_starts_text', __( 'Auction starts in:', 'wc_simple_auctions' ), $product); ?> 
+	<div class="auction-time future bid-stats" id="countdown"><?php echo apply_filters('auction_starts_text', __( 'Auction starts in:', 'wc_simple_auctions' ), $product); ?> 
 		<div class="auction-time-countdown future" data-time="<?php echo $product->get_seconds_to_auction() ?>" data-format="<?php echo get_option( 'simple_auctions_countdown_format' ) ?>"></div>
 	</div>
 	
